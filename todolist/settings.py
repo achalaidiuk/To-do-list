831055e19afb88cfad691cbf9d1c2455fd23149a
achalaidiuk <sasha.treyser90@gmail.com>
2024-09-25 11:24:49 +0300

    Feature: created view logic, forms.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist'
-    'todolist_project'
+    'todolist_project',
+    'todolist',
 ]
 
 MIDDLEWARE = [
--- todolist/urls.py
+++ todolist/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("todo.urls"), name="todo"),
+    path("", include("todolist_project.urls"), name="todolist_project"),
     path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
